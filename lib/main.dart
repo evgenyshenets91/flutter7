ae57aa362c0aa8a5b01402606278dd02deab9a7b
Evgeny Shenets <evgenyshenets91@gmail.com>
2020-12-26 23:37:59 +0300

    add initial loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter7/screens/LoadingScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +11,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(),
-      home: Container(),
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
     );
   }
 }
